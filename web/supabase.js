1f40d31ba28d59cb436bf90eea3b2639aa972404
Matt Sykes <matt.sykes@hotmail.co.uk>
2025-03-01 01:23:58 +0000

    Update Supabase Domain.

--- web/supabase.js
+++ web/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://vtrnebhplomlxakwhgpn.supabase.co', `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`)
+const supabase = createClient('https://pitmslrfogqaymkyfwhd.supabase.co', `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`)
 
 export { supabase }
